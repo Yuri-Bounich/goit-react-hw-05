3bc94ef2af64926567d4d6c5de5f7130ed7c2938
Yuri Bounich <b52ybunich@gmail.com>
2024-11-20 11:46:39 +0200

    navigation

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <>
+    <BrowserRouter>
       <App />
       <Toaster position="top-left" reverseOrder={true} />
-    </>
+    </BrowserRouter>
   </StrictMode>
 );
